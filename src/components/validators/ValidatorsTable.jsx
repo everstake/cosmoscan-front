077b4f952d1a87f3a3cbae8daad0f01c7907cb5d
shotgunofdeath <faust.child@gmail.com>
2020-06-30 17:42:48 +0300

    Validators stats sorting is added

--- src/components/validators/ValidatorsTable.jsx
+++ src/components/validators/ValidatorsTable.jsx
@@ -1,18 +1,18 @@
 import React, { useMemo } from 'react';
 import Table from '../Table';
 import useRequest from '../../hooks/useRequest';
-import { formatATOM, formatNum } from '../../utils';
+import { formatNum } from '../../utils';
 import API from '../../api';
 
 
 const cols = [
   { value: 'title', label: 'Validator' },
-  { value: 'power', label: 'Voting power' },
-  { value: 'selfStake', label: 'Self-stake' },
-  { value: 'fee', label: 'Fee' },
+  { value: 'power', label: 'Voting power (ATOM)' },
+  { value: 'selfStake', label: 'Self-stake (ATOM)' },
+  { value: 'fee', label: 'Fee (ATOM)' },
   { value: 'blocksProposed', label: 'Blocks proposed' },
   { value: 'delegators', label: 'Delegators' },
-  { value: 'powerChange', label: 'Stake change' },
+  { value: 'powerChange', label: 'Stake change (ATOM)' },
   { value: 'votes', label: 'Governance votes n/all' },
 ];
 
@@ -22,14 +22,14 @@ const ValidatorsTable = () => {
   const validators = useMemo(() => {
     if (!resp || !resp.length) return [];
 
-    return resp.map((validator) => ({
+    return resp.sort((a, b) => +b.power - +a.power).map((validator) => ({
       title: validator.title,
-      power: formatATOM(+validator.power),
-      selfStake: formatATOM(+validator.self_stake),
-      fee: formatATOM(validator.fee),
+      power: formatNum(+validator.power),
+      selfStake: formatNum(+validator.self_stake),
+      fee: formatNum(+validator.fee),
       blocksProposed: formatNum(validator.blocks_proposed),
       delegators: formatNum(validator.delegators),
-      powerChange: formatATOM(validator.power_24_change),
+      powerChange: formatNum(+validator.power_24_change),
       votes: formatNum(validator.governance_votes),
     }));
   }, [resp]);
